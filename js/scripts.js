7af340ce3dd60f01b6198adc45a17db4923f5890
wey46 <wyu0618@gmail.com>
2017-12-28 13:24:22 -0500

    1.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $("#mycarousel").carousel({interval:2000});
+
     $("#carousel-button").click(function(){
         if($("#carousel-button").children("span").hasClass('fa-pause')){
             $("#mycarousel").carousel('pause');
@@ -10,5 +11,21 @@ $(document).ready(function(){
             $("#carousel-button").children("span").removeClass('fa-play');
             $("#carousel-button").children("span").addClass('fa-pause');
         }
+    });
+
+    $("#loginBtn").click(function(){
+        $('#loginModal').modal('show')
+    });
+
+    $("#rsvBtn").click(function(){
+        $('#rsvmodal').modal('show')
+    });
+
+    $(".cancelBtn").click(function(){
+        $('#loginModal').modal('hide')
+    });
+
+    $(".cancelBtn2").click(function(){
+        $('#rsvmodal').modal('hide')
     });
 });
